4d55c9b3dcc6c2d447b533f74edb6623a7b0afe7
Adriano <jld.adriano@gmail.com>
2024-07-04 12:02:43 -0700

    Add delete-untracked-interactive alias

--- random-aliases.sh
+++ random-aliases.sh
@@ -1,2 +1,18 @@
 #/usr/bin/env zsh
 alias configs="cursor $(dirname $0)"
+function delete-untracked-interactive() {
+    git reset
+    #fzf loop to delete untracked files
+    while true; do
+        untracked_files=$(git ls-files --others --exclude-standard)
+        if [ -z "$untracked_files" ]; then
+            echo "No untracked files to delete."
+            return
+        fi
+        file=$(echo "$untracked_files" | fzf --prompt="Select file to delete (Ctrl+C to exit): ")
+        if [ -z "$file" ]; then
+            break
+        fi
+        rm "$file"
+    done
+}
