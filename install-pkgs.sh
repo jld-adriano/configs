5e72a9ba029171934b0d8f54d44e890516547832
Adriano <jld.adriano@gmail.com>
2024-07-05 16:46:08 -0700

    Home manager stuff

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -3,44 +3,49 @@
 set -eo pipefail
 
 # Nix base setup
-sh <(curl -L https://nixos.org/nix/install)
+if [ ! -f /etc/nix/nix.conf ]; then
+  sh <(curl -L https://nixos.org/nix/install)
+fi
+
 sudo rm -rf /etc/nix/nix.conf
 sudo ln -s $(pwd)/nix.conf /etc/nix/nix.conf
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
+local_home_manager_dir="$(pwd)/home-manager"
+user_home_manager_config_dir="$HOME/.config/home-manager"
+# Home manager setup
+rm -rf $user_home_manager_config_dir
+mkdir -p $user_home_manager_config_dir
+ln -s $local_home_manager_dir/home.nix $user_home_manager_config_dir/home.nix
+ln -s $local_home_manager_dir/flake.nix $user_home_manager_config_dir/flake.nix
+ln -s $local_home_manager_dir/flake.lock $user_home_manager_config_dir/flake.lock
+nix run home-manager/release-24.05 -- switch --flake $local_home_manager_dir#home
 
-nix-shell '<home-manager>' -A install
 
-rm -rf ~/.config/home-manager/home.nix
-ln -s $(pwd)/home.nix ~/.config/home-manager/home.nix
-
-
-# Home manager darwin
-mkdir -p ~/.config/nix
-rm -rf ~/.config/nix/flake.nix
-(cd ~/.config/nix && nix flake init -t nix-darwin)
-rm -rf ~/.config/nix/flake.nix
-ln -s $(pwd)/flake.nix ~/.config/nix/flake.nix
+# # Home manager darwin
+# mkdir -p ~/.config/nix
+# rm -rf ~/.config/nix/flake.nix
+# (cd ~/.config/nix && nix flake init -t nix-darwin)
+# rm -rf ~/.config/nix/flake.nix
+# ln -s $(pwd)/flake.nix ~/.config/nix/flake.nix
 
 # Atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+# curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
 
-#starship
-curl -sS https://starship.rs/install.sh | sh
+# #starship
+# curl -sS https://starship.rs/install.sh | sh
 
-# Install Homebrew packages
-brew install jq curl neovim fzf starship reflex github asdf
-brew install jfryy/tap/qq 
+# # Install Homebrew packages
+# brew install jq curl neovim fzf starship reflex github asdf
+# brew install jfryy/tap/qq 
 
-asdf plugin add nodejs
-asdf plugin add pnpm
-asdf install nodejs 20.11.0
+# asdf plugin add nodejs
+# asdf plugin add pnpm
+# asdf install nodejs 20.11.0
 
-volta install node
+# volta install node
 
-# Install casks
-brew install --cask cursor raycast
+# # Install casks
+# brew install --cask cursor raycast
 
-xargs -n 1 cursor --install-extension < vscode/extensions.list
+# xargs -n 1 cursor --install-extension < vscode/extensions.list
