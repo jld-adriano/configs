32fcd2072f6460eb2fa485ee9e7d3d7ebd09e198
Adriano <jld.adriano@gmail.com>
2024-07-05 14:43:40 -0700

    Add raycast scripts

---
+++ raycast-scripts/move-window-space-left.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Move window space left
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon :window:
+
+./.build/debug/raycast-scripts moveToPreviousSpace
+
