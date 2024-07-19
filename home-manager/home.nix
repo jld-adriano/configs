93cf5e2bbd18b97331be720c5d7b12424b52d187
Adriano <jld.adriano@gmail.com>
2024-07-19 14:42:13 -0700

    add some tmux configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -429,6 +429,24 @@ in {
     };
     oh-my-zsh = { enable = true; };
   };
+  
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      # Mouse works as expected
+      set-option -g mouse on
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+    '';
+  };
 
   programs.starship = {
     enable = true;
