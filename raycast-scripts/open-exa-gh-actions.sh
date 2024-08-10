4e693f93a868a86c140803c652da6c40d3a2946d
Adriano <jld.adriano@gmail.com>
2024-08-10 15:37:27 -0700

    add open-exa-gh-actions.sh

---
+++ raycast-scripts/open-exa-gh-actions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open exa github actions
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+open https://github.com/exa-labs/monorepo/actions
