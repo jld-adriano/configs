cf93196a8a1745ce40854fcd5e07e5f4cd534249
Adriano <jld.adriano@gmail.com>
2024-09-30 14:38:11 -0700

    add open-metrics.sh

---
+++ raycast-scripts/open-metrics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open exa metrics
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon :cloud:
+
+
+open https://grafana.exa.sh/d/main/vulcan?orgId=1
