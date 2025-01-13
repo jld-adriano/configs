b2ec7a4059477841578bdbe59ae5523967d87e9b
Adriano <jld.adriano@gmail.com>
2025-01-13 18:10:45 +0800

    add open clickhouse script

---
+++ raycast-scripts/open-clickhouse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open clickhouse
+# @raycast.mode silent
+# @raycast.keyboardShortcut ⌘⇧C
+
+# Optional parameters:
+# @raycast.icon ☁️
+
+open https://console.clickhouse.cloud/
