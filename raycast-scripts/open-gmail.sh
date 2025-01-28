d0c793b849f6e2557cc02e49808f1989c3bd8e52
Adriano <jld.adriano@gmail.com>
2025-01-28 17:06:19 +0800

    add open gmail raycast shcut

---
+++ raycast-scripts/open-gmail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open gmail
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon :envelope:
+
+export PATH=$PATH:/Users/jldadriano/.nix-profile/bin
+open https://mail.google.com/mail/u/0/#inbox
